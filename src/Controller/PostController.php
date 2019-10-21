0aee317023ef2f446d0f7c4cf8e56d0f828d88dc
CyrilCheru <cy.cheru@gmail.com>
2019-10-21 13:43:35 +0200

    Creation du formulaire Post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,10 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Tag;
 use App\Entity\Post;
-use App\Repository\PostRepository;
 use App\Entity\Category;
+use App\Repository\PostRepository;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
@@ -23,12 +32,114 @@ class PostController extends AbstractController
     }
 
     /**
-     * @Route("/blog/{category_id}/{id}", name="post_show", requirements={"id" : "\d+"})
+     * @Route("/blog/{id}", name="post_show", requirements={"id" : "\d+"})
      */
     public function show(Post $post)
     {
         return $this->render('post/show.html.twig', [
             'post' => $post
+        ]);
+    }
+
+    /**
+     * Création d'un article
+     * 
+     * @Route("blog/create", name="post_create")
+     *
+     * @return void
+     */
+    public function create(Request $request, EntityManagerInterface $entityManagerInterface)
+    {
+        $post = new Post;
+
+        // $form = $this->createForm(PostType::class, $post);
+
+
+        $builder = $this->createFormBuilder($post)
+            ->add('title', TextType::class, [
+                'label' => "Titre de l'article",
+                'attr' => [
+                    'placeholder' => "Donnez un titre percutant à votre article"
+                ]
+            ])
+            ->add('introduction', TextType::class, [
+                'label' => "Votre introduction",
+                'attr' => [
+                    'placeholder' => "Donnez un texte court qui donne envie"
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => "Contenu",
+                'attr' => [
+                    'placeholder' => "Contenu de votre article"
+                ]
+            ])
+            ->add('image', UrlType::class, [
+                'label' => "URL de l'image",
+                'attr' => [
+                    'placeholder' => "Adresse web de votre image"
+                ]
+            ])
+            // ->add('category', ChoiceType::class, [
+            //     'choices'  => $categoryRepository->findAll(),
+            //     'choice_label' => function (Category $category) {
+            //         return strtoupper($category->getTitle());
+            //     },
+            //     'choice_value' => 'id',
+            //     'label' => "Catégorie de l'article",
+            //     // 'multiple' => true,
+            //     'expanded' => true,
+
+            // ]);  OU
+            // ->add('category', EntityType::class, [
+            //     'class' => Category::class,
+            //     'query_builder' => function (EntityRepository $er) {
+            //         return $er->createQueryBuilder('c')
+            //             ->orderBy('c.title', 'ASC');
+            //     },
+            //     'choice_label' => function (Category $category) {
+            //         return strtoupper($category->getTitle());
+            //     },
+            //     // 'choice_value' => 'id',
+            //     'label' => "Catégorie de l'article",
+            //     // 'multiple' => true,
+            //     'expanded' => true,
+
+            // ]); OU
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'title',
+                // 'choice_value' => 'id',
+                'label' => "Catégorie de l'article",
+                // 'multiple' => true,
+                // 'expanded' => true,
+
+            ])
+            ->add('tags', EntityType::class, [
+                'class' => Tag::class,
+                'choice_label' => 'title',
+                'label' => "Etiquettes de l'article",
+                'multiple' => true,
+                'expanded' => true,
+                'attr' => [
+                    'class' => 'inline'
+                ]
+            ]);
+
+        $form = $builder->getForm();
+
+        // dd($form->createView());
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManagerInterface->persist($post);
+            $entityManagerInterface->flush();
+        }
+
+        return $this->render('post/create.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 
