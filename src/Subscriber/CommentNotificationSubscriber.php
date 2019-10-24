e7082b27a9b371505fc9acacc24c1ed4541efa55
CyrilCheru <cy.cheru@gmail.com>
2019-10-24 16:46:46 +0200

    Mise en place du Mailer et gestion de l envoi de mail au post d un comment

--- src/Subscriber/CommentNotificationSubscriber.php
+++ src/Subscriber/CommentNotificationSubscriber.php
@@ -2,14 +2,42 @@
 
 namespace App\Subscriber;
 
+use App\Event\CommentAddedEvent;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mime\Email;
 
 class CommentNotificationSubscriber implements EventSubscriberInterface
 {
+    protected $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
     public static function getSubscribedEvents()
     {
         return [
-            'kernel.controller' => ['addDateToAttributes', 1]
+            'blog.comment.add' => 'sendNotificationEmail'
         ];
+    }
+
+    public function sendNotificationEmail(CommentAddedEvent $event)
+    {
+        $comment = $event->getComment();
+
+
+        // TODO : Send an email to admin
+        $email = new Email();
+        $email->from(("notification@blog-a-la-con.com"))
+            ->to("admin@admin.com")
+            ->subject("Nouveau commentaire sur votre blog")
+            ->html("
+            <h2>Nouveau commentaire posté par </h2>
+            <strong>{$comment->getAuthor()->getName()}</strong> a écrit un commentaire sue l'article {$comment->getPost()->getTitle()}
+            <p>{$comment->getContent()}</p>
+            ");
+
+        $this->mailer->send($email);
     }
 }
